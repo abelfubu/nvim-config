3c6040ed0a4c21feec3f1552b4e2e68889eae395
Abel de la Fuente <abelfubu@gmail.com>
2024-11-10 16:47:53 +0100

    chore: recover neovim start screen

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+local event = #vim.fn.argv() > 0 and "VeryLazy" or "UIEnter"
+
+vim.api.nvim_create_autocmd(event, {
+  once = true,
+  callback = function()
+    require("lazyvim.config").load("keymaps")
+  end,
+})
