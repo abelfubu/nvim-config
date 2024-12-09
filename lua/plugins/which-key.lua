f93e53056bace99a6f42054ccadd48b72eb93774
Abel de la Fuente <abelfubu@gmail.com>
2024-12-09 19:24:45 +0000

    feat: add custom which key menus

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "folke/which-key.nvim",
+    opts = {
+      spec = {
+        { "<leader>z", group = "zen", icon = { icon = "󰱔", color = "green" } },
+        { "<leader>tc", group = "coverage", icon = { icon = "", color = "blue" } },
+      },
+    },
+  },
+}
